4aed1a335c68afff23f4cead0a94927f0e6b9efd
MitchyCola <32910204+MitchyCola@users.noreply.github.com>
2021-10-30 15:50:06 -0400

    Update network-tag.sh

--- res/scripts/network-tag.sh
+++ res/scripts/network-tag.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
+DIR="../cache"
+
 if [[ $1 == mainnet ]]
 then
-  echo "--mainnet" > ../cache/network.tag
+  echo "--mainnet" > "$DIR/network.tag"
+  cat "$DIR/mainnet.addr" | jq -r 'keys[]' > "$DIR/payment.utxo"
+  cat "$DIR/mainnet.addr" | jq '.[] | .["value"] | .[]' > "$DIR/payment.ada"
 elif [[ $1 == testnet ]]
 then
-  echo "--testnet-magic 8" > ../cache/network.tag
+  echo "--testnet-magic 1097911063" > "$DIR/network.tag"
+  cat "$DIR/testnet.addr" | jq -r 'keys[]' > "$DIR/payment.utxo"
+  cat "$DIR/testnet.addr" | jq '.[] | .["value"] | .[]' > "$DIR/payment.ada"
 else
   printf "Not a supported network tag"
   exit 128
